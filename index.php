90745b92bb1d41add5b26594f593d727be86cbf0
klyupav <pasha.klyuchnikov@gmail.com>
2018-04-07 15:00:02 +0500

    Update v2

--- index.php
+++ index.php
@@ -1,31 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: klyupav
- * Date: 19.03.17
- * Time: 13:28
- */
 
+require 'autoload.php';
 
-require 'vendor/autoload.php';
+Use ParseIt\Watches2uCom;
 
-//$pathToDonorsDir = './donors';
-//$files = scandir( $pathToDonorsDir );
-//foreach ( $files as $file )
-//{
-//    if( preg_match('%^(.*?)\.php$%is', $file, $match) )
-//    {
-//        $DonorClass = $match[1];
-//        if (!class_exists( $DonorClass ))
-//        {
-//            require_once $pathToDonorsDir. DIRECTORY_SEPARATOR . "{$DonorClass}.php";
-//        }
-//    }
-//}
-use ParseIt\Parser;
+$watches = new Watches2uCom();
 
-$parser = new Parser();
+$sources = $watches->getSources('https://www.watches2u.com/ladies-skagen-watches.html');
 
-$data = $parser->parsingDonor('https://www.watches2u.com/ladies-skagen-watches.html', 'Watches2uCom');
-print_r($data);
-die();
+print_r($sources);
