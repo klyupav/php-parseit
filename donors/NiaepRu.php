d1c63ca9d055d35a94cb34dc0ce35a09d890d5c7
klyupav <pasha.klyuchnikov@gmail.com>
2018-04-30 18:52:52 +0500

    Added donors and export to csv

---
+++ donors/NiaepRu.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace ParseIt\Donor;
+
+use ParseIt\ParserLogger;
+use ParseIt\_String;
+use ParseIt\nokogiri;
+use ParseIt\simpleParser;
+
+Class NiaepRu extends simpleParser {
+
+    public $data = [];
+    public $reload = [];
+    public $cache = false;
+    public $proxy = false;
+    public $cookieFile = '';
+    public $donor = 'NiaepRu';
+    public $project = 'archive.niaep.ru';
+    public $project_link = 'archive.niaep.ru';
+
+    function __construct()
+    {
+        $this->cookieFile = __DIR__.'/cookie/'.$this->donor.'/'.$this->donor.'.txt';
+    }
+
+    public function getSources($var = [])
+    {
+        $sources = [];
+        $content = $this->loadUrl('http://archive.niaep.ru/journalist/news/', $var);
+        if ( !$content )
+        {
+            return $sources;
+        }
+        if (preg_match('%url:[\s]*\'([^\']+)\'%uis', $content, $match))
+        {
+            $uri = explode('/', $match[1]);
+            $hash = $uri[count($uri)-1];
+            $content = $this->loadUrl('http://archive.niaep.ru/journalist/news/'.$hash, $var);
+            if ( !$content )
+            {
+                return $sources;
+            }
+            $js_string = $content;
+            $js_string = html_entity_decode($js_string);
+            $js_string = str_replace('"', '\"', $js_string);
+            $js_string = str_replace('\'', '"', $js_string);
+            $js_string = str_replace('title:', '"title":', $js_string);
+            $js_string = str_replace('href:', '"href":', $js_string);
+            $js_string = str_replace('date:', '"date":', $js_string);
+            $js_string = str_replace('author:', '"author":', $js_string);
+            $js_string = str_replace('picture:', '"picture":', $js_string);
+            $js_string = str_replace('annotation:', '"annotation":', $js_string);
+            $js_string = str_replace('new Date("', '"', $js_string);
+            $js_string = str_replace('"),', '",', $js_string);
+            $js_string = str_replace("\r", '', $js_string);
+            $js_string = str_replace("\n", '', $js_string);
+            $js_string = str_replace("	", '', $js_string);
+            $js_string = str_replace("\"\"", '" "', $js_string);
+            $sourceArray = json_decode($js_string);
+            foreach ($sourceArray as $item)
+            {
+                $uri = explode('/', $item->href);
+                $hash = $uri[count($uri)-1];
+                $sources[] = [
+                    'source' => 'http://archive.niaep.ru/journalist/news/',
+                    'href' => "http://archive.niaep.ru/journalist/news/".$hash,
+                    'title' => $item->title,
+                    'date' => date('Y-m-d H:i:s', strtotime($item->date)),
+                    'author' => $item->author,
+                    'pic_main' => !empty(trim($item->picture)) ? $this->fixUrl($item->picture) : '',
+                ];
+            }
+        }
+
+        return $sources;
+    }
+
+    public function getNewsDomMethod($url, $source = [])
+    {
+        $news = false;
+        $source['referer'] = $url;
+        $content = $this->loadUrl($url, $source);
+        if ( !$content )
+        {
+            return $news;
+        }
+        $content = preg_replace('%<span>[^<]*</span>\s*<br[^>]*>\s*<br[^>]*>%uis','',$content);
+//        $content = preg_replace('%<p[^>]*align="right"[^>]*>.*?</p>%uis','',$content);
+        $nokogiri = new nokogiri($content);
+        $text = trim(@$nokogiri->get('ul.raNewsList li')->toArray()[0]['__ref']->nodeValue);
+        $imgs = @$nokogiri->get('ul.raNewsList li img')->toArray();
+        $pic = '';
+        foreach ($imgs as $img)
+        {
+            if( isset($img['src']) && !empty(trim($img['src'])) )
+            {
+                $pic .= $this->fixUrl($img['src'])."|";
+            }
+        }
+        $news = [
+            'source' => $url,
+            'title' => $source['title'],
+            'date' => $source['date'],
+            'author' => $source['author'],
+            'text' => $text,
+            'pic_main' => $source['pic_main'],
+            'pic' => trim($pic,'|'),
+        ];
+
+        return $news;
+    }
+
+    public function getNewsRegexMethod($url, $source = [])
+    {
+        $news = false;
+        $source['referer'] = $url;
+        $content = $this->loadUrl($url, $source);
+        if ( !$content )
+        {
+            return $news;
+        }
+        $content = preg_replace('%<span>[^<]*</span>\s*<br[^>]*>\s*<br[^>]*>%uis','',$content);
+        $nokogiri = new nokogiri($content);
+        $imgs = @$nokogiri->get('ul.raNewsList li img')->toArray();
+        $pic = '';
+        foreach ($imgs as $img)
+        {
+            if( isset($img['src']) && !empty(trim($img['src'])) )
+            {
+                $pic .= $this->fixUrl($img['src'])."|";
+            }
+        }
+        $text = '';
+        if ( preg_match('%<ul[^>]*class="raNewsList[^>]*>.*?<li>(.*?)</li>%uis', $content, $match) )
+        {
+            $text = trim($match[1]);
+            $text = str_replace('<br>', "\r\n", $text);
+            $text = preg_replace('%<\!\-\-.*?\-\->%uis', "", $text);
+            $text = preg_replace('%<style>.*?</style>%uis', "", $text);
+            $text = preg_replace('%<[^>]*>%uis', "", $text);
+            $text = preg_replace('%[\r\n]+%uis', "\r\n", $text);
+            $text = trim($text);
+            $text = html_entity_decode($text);
+        }
+
+        $news = [
+            'source' => $url,
+            'title' => $source['title'],
+            'date' => $source['date'],
+            'author' => $source['author'],
+            'text' => $text,
+            'pic_main' => $source['pic_main'],
+            'pic' => trim($pic,'|'),
+        ];
+
+        return $news;
+    }
+}
--- index.php
+++ index.php
@@ -2,98 +2,80 @@
 
 require 'autoload.php';
 
-Use ParseIt\Donor\GlobaldriveRu;
+//http://www.atomstroyexport.ru/journalists/press/
 
-$config = new \Doctrine\DBAL\Configuration();
-$connectionParams = array(
-    'dbname' => 'vladmir_globaldrive',
-    'user' => 'kotopec',
-    'password' => '30031990',
-    'host' => 'localhost',
-    'driver' => 'pdo_mysql',
-);
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
-
-if ( isset($_GET['update']) )
+$atom = new \ParseIt\Donor\AtomstroyexportRu();
+$atom_s = $atom->getSources();
+$fp = fopen('1.atomstroyexport.csv','w');
+fwrite($fp, "№,источник,заголовок,дата,текст,фотки,основная фотка\r\n");
+foreach ( $atom_s as $k => $source)
 {
-    $conn->query("UPDATE `categories` SET `status`=0");
-    die('Status updated. Remove "?update" from url');
+    $N = $k+1;
+//    $news = $atom->getNewsDomMethod($source['href'], $source);
+//    $source['href'] = 'http://www.atomstroyexport.ru/journalists/press/3e4392004d20e8deb331bf8ad73eecf7';
+    $news = $atom->getNewsRegexMethod($source['href'], $source);
+    $src = $news['source'];
+    $title = str_replace('"', '""', $news['title']);
+    $date = str_replace('"', '""', $news['date']);
+    $text = str_replace('"', '""', $news['text']);
+    $pic = str_replace('"', '""', $news['pic']);
+    $pic_main = str_replace('"', '""', $news['pic_main']);
+    $csvstr = "{$N},\"{$src}\",\"{$title}\",\"{$date}\",\"{$text}\",\"{$pic}\",\"{$pic_main}\"\r\n";
+    fwrite($fp, $csvstr);
+//    break;
+//    print_r($csvstr);die();
+//    die();
 }
+fclose($fp);
 
-$donor = new GlobaldriveRu();
-$cats = $conn->query("SELECT * FROM categories WHERE status=0 LIMIT 1");
-while ($cat = $cats->fetch())
+//http://archive.niaep.ru/journalist/news/
+
+$niaep = new \ParseIt\Donor\NiaepRu();
+$niaep_s = $niaep->getSources();
+$fp = fopen('2.niaep.csv','w');
+fwrite($fp, "№,источник,заголовок,дата,текст,фотки,основная фотка\r\n");
+foreach ( $niaep_s as $k => $source)
 {
-    $conn->update('categories', ['status' => 1,], ['id' => $cat['id']]);
-    $category = $cat['link'];
-    $category_id = $cat['id'];
-    $sources = $donor->getSources(['url' => $category]);
-
-    foreach ( $sources as $source )
-    {
-        $product = $donor->getProductInfo( $source['url'] );
-        try
-        {
-            $results = $conn->query("SELECT * FROM products WHERE source='{$product['source']}'");
-            if ( $results->rowCount() > 0 )
-            {
-                $product_id = $results->fetch()['id'];
-                $conn->update('products', [
-                    'source' => $product['source'],
-                    'name' => $product['name'],
-                    'price' => $product['price'],
-                    'old_price' => $product['old_price'],
-                    'stock' => $product['stock'],
-                    'model' => $product['model'],
-                    'images' => $product['images'],
-                    'video' => $product['video'],
-                    'description' => $product['description'],
-                    'category_id' => $category_id,
-                ], ['id' => $product_id]);
-            }
-            else
-            {
-                $conn->insert('products', [
-                    'source' => $product['source'],
-                    'name' => $product['name'],
-                    'price' => $product['price'],
-                    'old_price' => $product['old_price'],
-                    'stock' => $product['stock'],
-                    'model' => $product['model'],
-                    'images' => $product['images'],
-                    'video' => $product['video'],
-                    'description' => $product['description'],
-                    'category_id' => $category_id,
-                ]);
-                $product_id = $conn->lastInsertId();
-                foreach ( $product['characters'] as $character )
-                {
-                    $search = $conn->query("SELECT * FROM characters WHERE name='{$character['name']}' AND product_id='{$product_id}'");
-                    if ( $search->rowCount() > 0 )
-                    {
-                        $conn->update('characters', [
-                            'value' => $character['value'],
-                        ],[
-                            'name' => $character['name'],
-                            'product_id' => $product_id,
-                        ]);
-                    }
-                    else
-                    {
-                        $conn->insert('characters', [
-                            'name' => $character['name'],
-                            'value' => $character['value'],
-                            'product_id' => $product_id,
-                        ]);
-                    }
-                }
-            }
-        }
-        catch (\Exception $e)
-        {
-            print_r($product);die();
-        }
-    }
-    die('Not end... Need reload page');
+    $N = $k+1;
+//    $news = $niaep->getNewsDomMethod($source['href'], $source);
+//    $source['href'] = 'http://www.atomstroyexport.ru/journalists/press/3e4392004d20e8deb331bf8ad73eecf7';
+    $news = $niaep->getNewsRegexMethod($source['href'], $source);
+    $src = $news['source'];
+    $title = str_replace('"', '""', $news['title']);
+    $date = str_replace('"', '""', $news['date']);
+    $text = str_replace('"', '""', $news['text']);
+    $pic = str_replace('"', '""', $news['pic']);
+    $pic_main = str_replace('"', '""', $news['pic_main']);
+    $csvstr = "{$N},\"{$src}\",\"{$title}\",\"{$date}\",\"{$text}\",\"{$pic}\",\"{$pic_main}\"\r\n";
+    fwrite($fp, $csvstr);
+//    break;
+//    print_r($csvstr);die();
+//    die();
 }
-die('End! <a href="?update">Update</a> "status" categories for start parsing');
+fclose($fp);
+
+//http://archive.aep.ru/wps/wcm/connect/aep/main/presscenter/nnews/
+
+$aep = new \ParseIt\Donor\AepRu();
+$aep_s = $aep->getSources();
+$fp = fopen('3.aep.csv','w');
+fwrite($fp, "№,источник,заголовок,дата,текст,фотки,основная фотка\r\n");
+foreach ( $aep_s as $k => $source)
+{
+    $N = $k+1;
+//    $news = $aep->getNewsDomMethod($source['href'], $source);
+//    $source['href'] = 'http://archive.aep.ru/wps/wcm/connect/aep/main/presscenter/nnews/489news';
+    $news = $aep->getNewsRegexMethod($source['href'], $source);
+    $src = $news['source'];
+    $title = str_replace('"', '""', $news['title']);
+    $date = str_replace('"', '""', $news['date']);
+    $text = str_replace('"', '""', $news['text']);
+    $pic = str_replace('"', '""', $news['pic']);
+    $pic_main = str_replace('"', '""', $news['pic_main']);
+    $csvstr = "{$N},\"{$src}\",\"{$title}\",\"{$date}\",\"{$text}\",\"{$pic}\",\"{$pic_main}\"\r\n";
+    fwrite($fp, $csvstr);
+//    break;
+//    print_r($csvstr);die();
+//    die();
+}
+fclose($fp);
